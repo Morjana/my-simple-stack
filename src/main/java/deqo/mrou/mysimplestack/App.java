b6add202a1af80fffbf14ae9445a056ac9122e53
Morjana <morjana.roumili@univ-tlse3.fr>
2018-10-18 14:57:48 +0200

    First commit  : commit des fichiers

---
+++ src/main/java/deqo/mrou/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.mrou.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
